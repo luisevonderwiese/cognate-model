03c6bec8ad5f99369f20c6178464a00acbba1831
Luise Haeuser <haeusele@cascade-login.cluster>
2024-02-21 11:18:44 +0100

    Make experiment with new model work

--- result_analysis_cognate.py
+++ result_analysis_cognate.py
@@ -2,6 +2,8 @@ import os
 import json
 from lingdata import database
 from ete3 import Tree
+from tabulate import tabulate
+import matplotlib.pyplot as plt
 
 
 def rf_distance(t1, t2):
@@ -14,8 +16,25 @@ def rf_distance(t1, t2):
         return float('nan')
     return rf/max_rf
 
+def gq_distance(tree_name1, tree_name2):
+    if tree_name1 is None or tree_name2 is None:
+        return float('nan')
+    if tree_name1 != tree_name1 or tree_name2 != tree_name2:
+        return float("nan")
+    os.system("./bin/qdist " + tree_name1 + " " + tree_name2 + " >out.txt")
+    lines = open("out.txt").readlines()
+    if len(lines) < 2: #error occurred
+        return float('nan')
+    res_q = float(lines[1].split("\t")[-3])
+    qdist = 1 - res_q
+    os.remove("out.txt")
+    return qdist
+
 def substitution_rates(prefix, x):
-    with open(prefix + ".raxml.log", "r") as logfile:
+    file_path = prefix + ".raxml.log"
+    if not os.path.isfile(file_path):
+        return []
+    with open(file_path, "r") as logfile:
         lines = logfile.readlines()
     rates = []
     for line in lines:
@@ -41,25 +60,108 @@ def substitution_rates(prefix, x):
         print("Illegal x")
         return []
 
+def base_frequencies(prefix, x):
+    file_path = prefix + ".raxml.log"
+    if not os.path.isfile(file_path):
+        return []
+    with open(file_path, "r") as logfile:
+        lines = logfile.readlines()
+    frequencies = []
+    for line in lines:
+        if line.startswith("   Base frequencies"):
+            parts = line.split(": ")[1].split(" ")[:-1]
+            frequencies = [float(part) for part in parts]
+            break
+    if frequencies == []:
+        return []
+    final_f = []
+    cursor = 1
+    while cursor <= x:
+        final_f.append(frequencies[cursor-1])
+        cursor *= 2
+    return final_f
+
+
+
+
 out_dir = "data/results_cognate/"
+plots_dir = "data/plots_cognate"
+if not os.path.isdir(plots_dir):
+    os.makedirs(plots_dir)
 database.read_config("cognate_lingdata_config.json")
 df = database.data()
-big_results_dict = {}
+results = []
+gq_distances = {"BIN" : [], "COG": []}
 for i, row in df.iterrows():
     msa_prefix = "_".join([row["ds_id"], row["source"], row["ling_type"], row["family"]])
     glottolog_tree_path = row["glottolog_tree_path"]
-    small_results_dict = {}
+    r = []
+    r.append(row["ds_id"])
     ds_path = os.path.join(out_dir, msa_prefix)
-    for msa_type in os.listdir(ds_path):
+    for msa_type in ["bin", "prototype"]:
         msa_path = os.path.join(ds_path, msa_type)
         for run_name in os.listdir(msa_path):
+            if run_name.startswith("pythia"):
+                    continue
             best_tree_path = os.path.join(msa_path, run_name, "inference.raxml.bestTree")
-            if os.path.isfile(glottolog_tree_path)  and os.path.isfile(best_tree_path):
-                small_results_dict["RF_" + msa_type + "_" + run_name] = rf_distance(Tree(best_tree_path), Tree(glottolog_tree_path))
+            if glottolog_tree_path == glottolog_tree_path and os.path.isfile(glottolog_tree_path)  and os.path.isfile(best_tree_path):
+                d = gq_distance(best_tree_path, glottolog_tree_path)
+                r.append(d)
+                gq_distances[run_name].append(d)
+            else:
+                r.append(float("nan"))
             if run_name == "COG":
                 prefix = os.path.join(msa_path, run_name, "inference")
-                small_results_dict["substitution_rates"] = substitution_rates(prefix, row["max_values_prototype"])
+                r.append(substitution_rates(prefix, row["max_values_prototype"]))
+                r.append(base_frequencies(prefix, row["max_values_prototype"]))
 
-    big_results_dict[msa_prefix] = small_results_dict
+    results.append(r)
 
-print(big_results_dict)
+print(tabulate(results, tablefmt="pipe", floatfmt=".3f", headers = ["ds_id", "GQ BIN", "GQ COG", "subtitution rates", "base_frequencies"]))
+
+
+plt.axline([0, 0], slope=1, color = 'lightgray', linewidth = 1, linestyle = "--")
+plt.scatter(gq_distances["BIN"], gq_distances["COG"], s=10)
+plt.xlabel('BIN')
+plt.ylabel('COG')
+plt.savefig(os.path.join(plots_dir, "scatter_cognate.png")) 
+plt.clf()
+
+all_rates = [r[3] for r in results]
+max_num = max([len(rates) for rates in all_rates])
+fig,ax = plt.subplots(figsize=(40, 30))
+x = range(len(all_rates))
+y_old = [0 for el in x]
+for num in range(max_num):
+    y_new = []
+    for rates in all_rates:
+        if len(rates) > num:
+            y_new.append(rates[num] /sum(rates))
+        else:
+            y_new.append(0)
+    ax.bar(x, y_new, bottom=y_old, label = str(num))
+    for i in x:
+        y_old[i] = y_old[i] + y_new[i]
+ax.legend()
+plt.savefig(os.path.join(plots_dir, "stacked_substitution_rates.png"))
+plt.clf()
+
+all_rates = [r[4] for r in results]
+max_num = max([len(rates) for rates in all_rates])
+fig,ax = plt.subplots(figsize=(40, 30))
+x = range(len(all_rates))
+y_old = [0 for el in x]
+for num in range(max_num):
+    y_new = []
+    for rates in all_rates:
+        if len(rates) > num:
+            y_new.append(rates[num] / sum(rates))
+        else:
+            y_new.append(0)
+    ax.bar(x, y_new, bottom=y_old, label = str(num))
+    for i in x:
+        y_old[i] = y_old[i] + y_new[i]
+ax.legend()
+plt.savefig(os.path.join(plots_dir, "stacked_base_frequencies.png"))
+plt.clf()
+
